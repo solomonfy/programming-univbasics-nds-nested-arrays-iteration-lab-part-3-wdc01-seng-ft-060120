27c46c35f69cb6cf079ad64f074a24449a6123d8
SOLOMON YISMAW <fantahunsolomon@gmail.com>
2020-05-02 06:30:44 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,43 +1,47 @@
 require "pry"
-def join_nested_strings(src)
+#def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
 
-joined = []
-row_index = 0
-while row_index < src.count do
-  element_index = 0
-  while element_index < src[row_index].count do
-    if src[row_index][element_index].class == String
+#joined = []
+#row_index = 0
+#while row_index < src.count do
+#  element_index = 0
+#  while element_index < src[row_index].count do
+#    if src[row_index][element_index].class == String
       #binding.pry
-        phrase = src[row_index][element_index].join( ) 
-      joined << phrase  
-   element_index += 1
-  end
-end
-  row_index += 1
-end
-joined
-end
-
-
-#words = []
-#src.each do |items|
-#  items.each do |item|
-#    if item.class == String
-#      words << item
-#    end
+#        phrase = src[row_index][element_index].join( ) 
+#      joined << phrase  
+#   element_index += 1
 #  end
 #end
-
-#[words.join(" ")]
-
+#  row_index += 1
+#end
+#joined
 #end
 
-#[
-#  src.
-#    flatten.
-#    grep(String).
-#    join(' ') ]
 
-#end
+def join_nested_strings(src)
+  
+  new_string = ""
+  row_index = 0 
+  
+  while row_index < src.count do 
+    
+    element_index = 0 
+  
+    array = src[row_index]
+    while element_index < array.count  do 
+      
+      if src[row_index][element_index] == src[row_index][element_index].to_s 
+        new_string += src[row_index][element_index]
+        new_string += " "
+
+      end
+      element_index += 1 
+    
+		end
+    row_index +=1
+  end
+new_string
+end
